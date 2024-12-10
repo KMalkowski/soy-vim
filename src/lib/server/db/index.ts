d801edc710c6f6f3710c9dbfaeee213dbeaaa6d4
Krzysztof Malkowski <krzysztof.malkowski098@gmail.com>
2024-12-10 21:15:52 +0100

    dumb

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,6 +3,6 @@ import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { DATABASE_URL } from '$env/static/private';
 
-if (DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = await mysql.createConnection(DATABASE_URL);
 export const db = drizzle(client, { schema: { ...schema }, mode: 'default' });
